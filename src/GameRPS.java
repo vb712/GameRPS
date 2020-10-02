6ea54c580343589b8ee02a087db5ef0e66fd787b
vb712 <42868214+vb712@users.noreply.github.com>
2020-10-02 07:18:47 +0530

    Update GameRPS.java

--- src/GameRPS.java
+++ src/GameRPS.java
@@ -1,37 +1,61 @@
-import java.util.Scanner;
+		import java.util.Scanner;
+
 public class GameRPS {
 
-	public static void main(String[] args) {
-		
-		//Generate a random Rock, Paper or Scissors
-		int game =(int)(Math.random() * 2);
-		
-		//Prompt the user to enter a guess
-		Scanner input = new Scanner(System.in);
-		System.out.println("Let's play Rock - Paper - Scissors!!!");
-		System.out.println("Enter a number... Rock (0), Paper (1) or Scissors(2): ");
-		int guess = input.nextInt();
-		
-		//Check the guess
-		if (guess == game){
-			System.out.println("The computer picked: " + game + " .");
-			System.out.println("You picked: " + guess + " too.");
-			System.out.println("It's a tie!");
-		}
-		else if (game == 0 && guess == 1 || game == 1 && guess == 2 || game == 2 && guess == 0){
-			System.out.println("The computer picked: " + game + " .");
-			System.out.println("You picked: " + guess + " .");
-			System.out.println("You won!");
-		}	
-		else if (guess == 0 && game == 1 || guess == 1 && game == 2 || guess == 2 && game == 0){	
-			System.out.println("The computer picked: " + game + " .");
-			System.out.println("You picked: " + guess + " .");
-			System.out.println("You lose!");
-		}
-		else {
-			System.out.println("Game Over.");
-		}
-		}
-			
-		
-	}
+    public static void main(String[] args) {
+        int x = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Rock Paper Scissors game in Java!");
+        do {
+            System.out.println("To Play Type Rock Or Paper Or Scissors");
+            String ansp = sc.nextLine();
+            if (ansp.equalsIgnoreCase("Rock")) {
+                System.out.println("...");
+            } else if (ansp.equalsIgnoreCase("Paper")) {
+                System.out.println("...");
+            } else if (ansp.equalsIgnoreCase("Scissors")) {
+                System.out.println("...");
+            } else {
+                System.out.println("Error type Rock or Paper Or Scissors");
+                System.exit(0);
+            }
+            System.out.println("You Choose :" + ansp);
+            int rand = (int) Math.random() * 2;
+            if (rand == 0) {
+                System.out.println("Computer chose : Rock");
+            } else if (rand == 1) {
+                System.out.println("Computer chose : Paper");
+            } else if (rand == 2) {
+                System.out.println("Computer chose : Scissors");
+            }
+            if (rand == 0 && ansp.equalsIgnoreCase("Rock")) {
+                System.out.println("Draw!");
+                x++;
+            } else if (rand == 0 && ansp.equalsIgnoreCase("Paper")) {
+                System.out.println("You Won!");
+                x++;
+            } else if (rand == 0 && ansp.equalsIgnoreCase("Scissors")) {
+                System.out.println("Computer Won!");
+                x++;
+            } else if (rand == 1 && ansp.equalsIgnoreCase("Rock")) {
+                System.out.println("Computer Won!");
+                x++;
+            } else if (rand == 1 && ansp.equals("Paper")) {
+                System.out.println("Draw");
+                x++;
+            } else if (rand == 1 && ansp.equalsIgnoreCase("Scissors")) {
+                System.out.println("You Won!");
+                x++;
+            } else if (rand == 2 && ansp.equalsIgnoreCase("Rock")) {
+                System.out.println("You Won!");
+                x++;
+            } else if (rand == 2 && ansp.equalsIgnoreCase("Paper")) {
+                System.out.println("Computer Won!");
+                x++;
+            } else if (rand == 2 && ansp.equalsIgnoreCase("Scissors")) {
+                System.out.println("Draw!");
+                x++;
+            }
+        } while (x < 6);
+    }
+}
